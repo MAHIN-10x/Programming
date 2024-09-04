5e95546dcee41fa3a26e3ae99fa56c5778aabced
Mahin-10x <mahin2069@gmail.com>
2024-09-04 16:42:12 +0600

    Big number among two number

--- BigNumber.cpp
+++ BigNumber.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
-    printf("Hello World!");
+    int a = 7, b = 6;
+
+    if(a > b)
+    {
+        printf("%d is greater than %d", a, b);
+    }else{
+        printf("%d is greater than %d", b, a);
+    }
 
     return 0;
 }
