ce576c8347c61eba124b100d088f9b19359b9b83
MAHIN <mahin2069@gmail.com>
2024-09-09 01:19:09 +0600

    Decimal to Binary convert

---
+++ DtoB.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int decimalToBinary(int n) {
+
+    int pow = 1;
+    int answer = 0;
+    while(n > 0) {
+        int rem = n % 2;
+        n = n/2;
+        answer += rem * pow;
+        pow *= 10;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout<<decimalToBinary(5)<<endl;
+}
