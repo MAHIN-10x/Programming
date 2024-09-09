3aaba5d40ac118a0e363946dfc09c3d021492b75
MAHIN <mahin2069@gmail.com>
2024-09-10 04:59:50 +0600

    Check n is power of 2 or not

--- Bitwise.cpp
+++ Bitwise.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+bool isPowerOfTwo(int n) {
+    return (n>0 && (n & (n-1)) == 0);
+}
+
 
 int main()
 {
-    int a = 8, b = 2;
-    cout<<(a>>b)<<endl;
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if(isPowerOfTwo(num)) {
+        cout<<num<<" is a power of 2"<<endl;
+    } else {
+        cout<<num<<" is not a power of 2"<<endl;
+    }
+
+    return 0;
 }
