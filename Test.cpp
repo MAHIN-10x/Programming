a18efe64f55de58097a6468c049a47f15af9575e
MAHIN <mahin2069@gmail.com>
2024-09-10 05:00:54 +0600

    Find the value of nCr

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,28 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int fact(int n) {
+    if(n == 1) {
+        return 1;
+
+    } else {
+        return n * fact(n-1);
+    }
+}
 
 int main()
 {
-    printf("Hello World!");
+    int n, r;
+
+    cout<<"Input n and r: ";
+    cin>>n>>r;
+
+    int fact_n = fact(n);
+    int fact_r = fact(r);
+    int fact_nr = fact(n-r);
+    int fact_nCr = fact_n/(fact_r * fact_nr);
+
+    cout<< fact_nCr << endl;
 
     return 0;
 }
